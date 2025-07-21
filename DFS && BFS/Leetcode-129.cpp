bf16f934779c5a7af4a0a09a35aa47143c74321b
udayyyy-09 <udaychaudhary419@gmail.com>
2025-07-21 23:28:35 +0530

    dfs top-down 129

---
+++ DFS && BFS/Leetcode-129.cpp
@@ -0,0 +1,10 @@
+/*      129. Sum Root to Leaf Numbers          https://leetcode.com/problems/sum-root-to-leaf-numbers/
+You are given the root of a binary tree containing digits from 0 to 9 only.
+Each root-to-leaf path in the tree represents a number.
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+A leaf node is a node with no children.
+
+*/
+
+//  My Solution: https://leetcode.com/problems/sum-root-to-leaf-numbers/solutions/6875534/simple-approach-beats-100-users-by-udayy-plx2
